4ed8bafb2e63deae642de6597636ec4961fcdf19
Donald <don@tig.org>
2025-04-10 11:31:31 -0400

    Update reference to private registry

--- main.tf
+++ main.tf
@@ -3,9 +3,11 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/learn-terraform-3481/s3-webapp/aws"
+  version = "1.0.0"
+  # source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  # version = "1.0.0"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
-  version = "1.0.0"
 }
